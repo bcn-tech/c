f33f0f81128006f9ebacf6006c13b03ee0137afb
bcn-tech <80455891+bcn-tech@users.noreply.github.com>
2021-03-18 10:13:18 +0700

    Update data.ts

--- data.ts
+++ data.ts
@@ -1,6 +1,6 @@
 export default {
-  siteUrl: "https://mysite.com",
-  name: "MVP Starter",
+  siteUrl: "https://techinlabs.co",
+  name: "Techinlabs",
   tagline: "Craft your MVP in under 5 minutes!",
   description:
     "MVP starter is a template made for the busy minded maker. You take care of the copy, and the template takes care of the rest for you.",
